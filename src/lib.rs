49c3d8b4d6d3d3d7d2a9ec24ae53d6a1ee3d76fa
Chad Sharp <cmlsharp@umich.edu>
2025-12-05 16:23:55 -0500

    fix borrow debug_assert bug

--- src/lib.rs
+++ src/lib.rs
@@ -221,7 +221,7 @@ impl U256 {
     /// If `borrow` is true, this is equal to `self - rhs`
     pub fn borrowing_sub(&self, rhs: &Self) -> (Self, bool) {
         let (res, Limb(borrow)) = self.0.sbb(&rhs.0, Limb::ZERO);
-        debug_assert!(borrow == 0 || borrow == 1);
+        debug_assert!(borrow == 0 || borrow == u64::MAX);
         (U256(res), borrow != 0)
     }
 }
